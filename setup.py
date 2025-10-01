0b58a4d2c0c5720b8ba027f975ce82abb13fe96e
Duo <edintahiri.2013@gmail.com>
2025-10-01 22:25:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-	name="TerminalOS",
-	version="1.0",
-	packages=find_packages(),
-	install_requires=[
-		"PyQt6",
-		"requests",
-		"pyfiglet"
-	],
-	entry_points={
-		"console_scripts": [
-			"terminalos=core.terminal:main",
-		],
-	},
-	author="Edin",
-	author_email="edintahiri.2013@gmail.com"
-	description="TerminalOS - python app"
-	url="https://github.com/Zypher0903/TerminalOS",
+    name="TerminalOS",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        "PyQt6",
+        "requests",
+        "pyfiglet"
+    ],
+    entry_points={
+        "console_scripts": [
+            "terminalos=main:main",
+        ],
+    },
+    author="Your Name",
+    author_email="edintahiri.2013@gmail.com",
+    description="TerminalOS - A Python-based terminal application",
+    url="https://github.com/Zypher0903/TerminalOS",
 )
